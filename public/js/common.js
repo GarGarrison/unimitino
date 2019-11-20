c8a492039fc4577e40e25aa3c77751cf23be2575
garrison <garrisongar@gmail.com>
2019-11-20 16:39:31 +0300

    fixed sticky nav menu responsive bug

--- public/js/common.js
+++ public/js/common.js
@@ -12,17 +12,24 @@ $(document).ready(function(){
 });
 
 $(document).scroll(function(){
+    var top_val = 73;
+    var position = 'absolute'
+    if (window.outerWidth < 992) {
+        top_val = 0;
+        position = 'initial';
+    }
+
     var nav = $("nav.top-nav");
-    if (window.scrollY > 73) {
+    if (window.scrollY > top_val) {
         nav.css({
             'position': "fixed",
-            'top': '0',
+            'top': 0,
         });
     }
     else {
         nav.css({
-            'position': 'absolute',
-            'top': 73
+            'position': position,
+            'top': top_val
         });
     }
 });
